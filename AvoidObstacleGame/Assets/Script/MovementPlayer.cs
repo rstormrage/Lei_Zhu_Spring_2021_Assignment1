ba998ad1e9d59de5f54eb0a2d138065c2539be68
Lei Zhu <zhu29@marshall.edu>
2021-03-15 20:52:03 -0400

    Assignment

---
+++ AvoidObstacleGame/Assets/Script/MovementPlayer.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rigidBody;
+    public float forwardForce = 1500;
+    public float sideForce = 25;
+
+    void Start()
+    {
+        
+
+        //rigidBody.useGravity = false;
+
+        rigidBody.AddForce(0, 0, 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidBody.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidBody.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+    }
+
+    void FixedUpdate() 
+    {
+        rigidBody.AddForce(0, 0, 1500 * Time.deltaTime);
+
+    }
+
+}
---
+++ AvoidObstacleGame/Assets/Script/PlayerScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PlayerScore : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(player.position.z);
+
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
